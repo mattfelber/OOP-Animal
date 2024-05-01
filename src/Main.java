6afbe814f65f6edcf7d87d7a6a1a505fe5d48b1b
Matheus Felber <math.felber@gmail.com>
2024-04-30 21:58:22 -0300

    OOP Animal

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.util.LinkedList;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Polar Bear:");
         PolarBear polarBear = new PolarBear();
         polarBear.surface();
@@ -17,10 +14,6 @@ public class Main {
         jaguar.Eat();
         jaguar.Move();
         jaguar.Reproduce();
-
-
-
         }
-
 
 }
